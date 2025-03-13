d6d05622f88a95bc4cb4ee2d1ecf9c1fcf254df8
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-13 11:09:07 +0700

    Create home.dart

---
+++ flutter_apps/lib/acara21/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
+            colors: [
+              Colors.white,
+              Colors.purpleAccent,
+              Colors.deepPurple,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
